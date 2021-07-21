e1d94fa689b9b7f7bae9e01fab2c92507199667f
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-21 09:34:55 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,18 +8,33 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
 
-app.get('/', (req, res) => {
-res.json({
-    message: 'Hello World'
-    });
-  });
 
-  // Default response for any other request (Not Found)
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Lemon123',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
